924faa95b1badf3ccfac2d0bb8971d14004b98ef
AndyHazz <517731+AndyHazz@users.noreply.github.com>
2021-08-17 23:27:14 +0100

    Update logo-resize.bat

--- logo-resize.bat
+++ logo-resize.bat
@@ -1,13 +1,20 @@
 MD logos-resized
 MD logos-arcadeitalia
+MD logos-for-cpwizard
+
+@REM FOR %%r IN ("C:\Users\andyn\Documents\MAME 0.106 Non-Merged\roms\*.zip") DO (
+@REM   if EXIST "logos-original\%%~nr.png" (
+@REM     magick "logos-original\%%~nr.png" -background none -trim -resize 140x105^> -gravity south -extent 140x105 "logos-resized\%%~nr.png"
+@REM   ) else (
+@REM     wget "http://adb.arcadeitalia.net/media/mame.current/decals/%%~nr.png" -P "logos-arcadeitalia"
+@REM     magick "logos-arcadeitalia\%%~nr.png" -background none -trim -resize 140x105^> -gravity south -extent 140x105 "logos-resized\%%~nr.png"
+@REM   )
+@REM )
 
 FOR %%r IN ("C:\Users\andyn\Documents\MAME 0.106 Non-Merged\roms\*.zip") DO (
   if EXIST "logos-original\%%~nr.png" (
-    @REM magick "logos-original\%%~nr.png" -background none -trim -resize 140x105^> -gravity south -extent 140x105 "logos-resized\%%~nr.png"
-  ) else (
-    wget "http://adb.arcadeitalia.net/media/mame.current/decals/%%~nr.png" -P "logos-arcadeitalia"
-    magick "logos-arcadeitalia\%%~nr.png" -background none -trim -resize 140x105^> -gravity south -extent 140x105 "logos-resized\%%~nr.png"
-  )
+    magick "logos-original\%%~nr.png" -background none -trim -resize 160x64^> -gravity north -extent 160x64 "logos-for-cpwizard\%%~nr.png"
+  ) 
 )
 
 @REM MD marquees
